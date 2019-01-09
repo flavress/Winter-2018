85a781c87f20a274d445fec3b339f3529277e93a
Raj Balasubramanian <schroskat@gmail.com>
2019-01-09 13:52:56 -0600

    first commit on ex1

---
+++ ex1.py
@@ -0,0 +1,9 @@
+def Fullname( Firstname, Lastname ):
+  print (Firstname.capitalize() +" "+Lastname.capitalize())
+  return Firstname.capitalize() +" "+Lastname.capitalize()
+
+
+def Systempreferred ( Lastname, Firstname ):
+  print (Lastname.capitalize()+", "+Firstname.capitalize())
+  return Lastname.capitalize()+", "+Firstname.capitalize()
+  
