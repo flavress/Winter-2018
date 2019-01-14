28d23439b4364960056eab7951a6d4ce6b6fed38
flavress <46035247+flavress@users.noreply.github.com>
2019-01-14 13:26:05 -0600

    familyexample.py

---
+++ Familyexample.py
@@ -0,0 +1,7 @@
+Family = ["Raj Balasubramanian", "Stefanie Balasubramanian", "Samuel Balasubramanian","Jay Balasubramanian", "Rohan Balasubramanian"]
+for x in Family:
+...  print(x)
+
+Family_Sys = ["Balasubramanian, Raj", "Balasubramanian, Stefanie", "Balasubramanian, Samuel", "Balasubrmanian, Jay", "Balasubramanian, Rohan"]
+for x in Family_Sys:
+...  print(x)
